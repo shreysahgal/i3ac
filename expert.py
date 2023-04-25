15310c85b70aa2b638f793db0eacc932d4a4197e
Shrey Sahgal <shreysahgal@gmail.com>
2023-04-25 16:07:14 -0400

    iac without trained children or LP

--- expert.py
+++ expert.py
@@ -6,7 +6,7 @@ class Expert(nn.Module):
     def __init__(self):
         super(Expert, self).__init__()
         # input: 84x84 image + 12 action
-        # output: 84x84 image
+        # output: 84x84 imag
         self.fc1 = nn.Linear(84*84 + 12, 84*84)
         self.fc2 = nn.Linear(84*84, 84*84)
         self.fc3 = nn.Linear(84*84, 84*84)
@@ -14,5 +14,5 @@ class Expert(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = F.relu(self.fc3(x))
         return x.float()
--- iac.py
+++ iac.py
@@ -3,32 +3,133 @@ from expert import Expert
 import torch
 from sklearn.preprocessing import OneHotEncoder
 from time import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import gc
+
+# TODO: add region splitting
+# TODO: repeated actions DONE
+# TODO: calculate LP
+
+class Region:
+
+    def __init__(self, max_samples=100, centroids=np.zeros((1, 84*84 + 12))):
+
+        self.max_samples = max_samples
+        self.centroids = centroids
+        self.num_state_motor = 0
+        self.num_next_state = 0
+        self.num_error = 0
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.expert = Expert()
+
+        self.state_motor = np.zeros((self.max_samples, 84*84 + 12))
+        self.next_state = np.zeros((self.max_samples, 84*84))
+        self.error = np.zeros(self.max_samples)
+    
+    def inherit_from_parent(self, state_motor, next_state, num_samples):
+        self.state_motor[:num_samples] = state_motor
+        self.next_state[:num_samples] = next_state
+        print(num_samples)
+        self.num_state_motor = num_samples
+        self.num_next_state = num_samples
+        self.num_error = num_samples
+
+        # TODO: train child expert on given samples
+    
+    def add_state_motor(self, state, action):
+        action = torch.nn.functional.one_hot(torch.tensor(action), num_classes=12).float()
+        state_motor = np.concatenate((state.flatten(), action))
+        self.state_motor[self.num_state_motor] = state_motor
+        self.num_state_motor += 1
+    
+    def add_next_state(self, next_state):
+        self.next_state[self.num_next_state] = next_state.flatten()
+        self.num_next_state += 1
+    
+    def predict_state(self):
+        self.expert.eval()
+        X = torch.tensor(self.state_motor[self.num_state_motor-1]).float().to(self.device)
+        output = self.expert(X).detach().cpu().numpy()
+        X.to("cpu")
+        del X
+        return output
+
+    def update_expert(self, optimizer):
+        self.expert.train()
+        X = torch.tensor(self.state_motor[self.num_state_motor-1]).float().to(self.device)
+        y = torch.tensor(self.next_state[self.num_next_state-1]).float().to(self.device)
+
+        loss = torch.nn.MSELoss()
+        optimizer.zero_grad()
+        output = self.expert(X)
+        loss = loss(output, y)
+        loss.backward()
+        optimizer.step()
+        X.to("cpu")
+        y.to("cpu")
+        del X, y
+    
+    def update_error(self, error):
+        self.error[self.num_error] = error
+        self.num_error += 1
+        
+    def get_last_error(self):
+        return self.error[self.num_error-1]
+    
+    def get_num_samples(self):
+        assert self.num_state_motor == self.num_next_state == self.num_error
+        return self.num_state_motor
+
+    def split(self):
+        del self.expert
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(self.state_motor)
+        centroids = kmeans.cluster_centers_
+        clusters = kmeans.labels_
+
+        region1 = Region(self.max_samples, centroids[0])
+        region2 = Region(self.max_samples, centroids[1])
+
+        region1.inherit_from_parent(self.state_motor[clusters == 0], self.next_state[clusters == 0], np.sum(clusters == 0))
+        region2.inherit_from_parent(self.state_motor[clusters == 1], self.next_state[clusters == 1], np.sum(clusters == 1))
+
+        return region1, region2
+    
+    def should_split(self):
+        if self.num_state_motor >= self.max_samples:
+            return True
+        else:
+            return False
+    
+    def expert_to_gpu(self):
+        self.expert.to(self.device)
+    
+    def expert_to_cpu(self):
+        self.expert.to("cpu")
+
+        
 
 class IAC:
-    def __init__(self, env, sensory_dim=84*84):
+    def __init__(self, env, sensory_dim=84*84, split_threshold=100):
         self.env = env
         self.state = self.env.reset()
         self.action_space = self.env.action_space
         self.sensory_dim = sensory_dim
 
-        self.random_steps = 0
-        self.repeat_action = 4
+        self.random_steps = 1000
+        self.repeat_action = 2
 
         self.num_steps = 0
 
         self.n_regions = 1
-        self.centroids = np.zeros((1, self.sensory_dim))
+        self.regions = [Region()]
+
+        self.split_threshold = split_threshold
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.experts = [Expert().to(self.device)]
-        self.state_motor = [[]]
-        self.next_state = [[]]
-        self.error = [[]]
-    
-    # def transform_obs(self, obs):
-    #     obs = ResizeObservation(obs, (84, 84)).observation
-    #     obs = GrayScaleObservation(obs).observation
-        # return obs
+        self.optim = torch.optim.Adam(self.regions[0].expert.parameters(), lr=0.001)
     
     def reset(self):
         self.state = self.env.reset()
@@ -36,73 +137,100 @@ class IAC:
     
     def render(self):
         self.env.render()
-    
-    def select_region(self):
-        dists = np.linalg.norm(self.centroids - self.state.flatten(), axis=1)
-        return np.argmin(dists)
 
-    def select_action(self, region):
+    def select_region_action(self):
 
         # choose a random action for the first self.num_steps
         if self.num_steps < self.random_steps:
-            return self.env.action_space.sample()
+            action = self.env.action_space.sample()
+            if self.n_regions < 2:
+                region = 0
+            else:
+                action_vec = np.zeros(self.action_space.n)
+                action_vec[action] = 1
+                SM_t = np.concatenate((self.state.flatten(), action_vec))
+                # for each region in self.regions calculate the distance to the state-action vector, find min dists
+                dists = np.zeros(self.n_regions)
+                for i in range(self.n_regions):
+                    dists[i] = np.linalg.norm(self.regions[i].centroids - SM_t)
+                region = np.argmin(dists)
+            return region, action
 
+        # TODO: update this with new regions
+        """ 
         # list of learning progress values for each action
         lp_list = np.zeros(self.action_space.n)
 
         for i, action in enumerate(list(range(self.action_space.n))):
             # TODO: implement the observation space feature encoding here
-            SM_t = np.concatenate((self.state.flatten(), np.array([action])))
+            action_vec = np.zeros(self.action_space.n)
+            action_vec[action] = 1
+            SM_t = np.concatenate((self.state.flatten(), action_vec))
+
+            dists = np.linalg.norm(self.centroids - SM_t, axis=1)
+            region = np.argmin(dists)
 
             # determine expected learning progress
             lp_list[i] = self.calculate_lp(region, self.state, action)
         
         action = np.argmax(lp_list)
-        return action
+        return region, action
+        """
     
     def calculate_lp(self, region, state, action):
         return None
-    
-    def predict_state(self, region, action):
-        self.experts[region].eval()
-        action = torch.nn.functional.one_hot(torch.tensor(action), num_classes=self.action_space.n).float()
-        X = torch.cat((torch.tensor(self.state.flatten()), action)).to(self.device)
-        return self.experts[region](X).detach().cpu().numpy()
-    
-    def update_expert(self, region, state, action, next_state):
-        self.experts[region].train()
-        action = torch.nn.functional.one_hot(torch.tensor(action), num_classes=self.action_space.n).float()
-        X = torch.cat((torch.tensor(state.flatten()), action)).to(self.device)
-        y = torch.tensor(next_state.flatten()).float().to(self.device)
-        loss = torch.nn.MSELoss()
-        optimizer = torch.optim.Adam(self.experts[region].parameters(), lr=0.001)
-        optimizer.zero_grad()
-        output = self.experts[region](X)
-        loss = loss(output, y)
-        loss.backward()
-        optimizer.step()
-    
+
     def step(self):
         # selection region and action
-        region = self.select_region()
-        action = self.select_action(region)
-        self.state_motor[region].append((self.state, action))
+        region_idx, action = self.select_region_action()
+
+        # load region expert onto gpu
+        self.regions[region_idx].expert.to('cuda')
+
+        # update region state motor
+        self.regions[region_idx].add_state_motor(self.state, action)
 
         # make prediction
-        predicted_state = self.predict_state(region, action)
+        predicted_state = self.regions[region_idx].predict_state()
 
         # step environment
-        next_state, reward, done, info = self.env.step(action)
-        self.next_state[region].append(next_state)
+        for _ in range(self.repeat_action):
+            next_state, reward, done, info = self.env.step(action)
 
-        self.num_steps += 1
+        # update region next state
+        self.regions[region_idx].add_next_state(next_state)
 
         # update error of the region
-        self.error[region].append(np.linalg.norm(predicted_state - next_state.flatten()))
-        self.update_expert(region, self.state, action, next_state)
-        
+        error = np.linalg.norm(predicted_state - next_state.flatten())
+        self.regions[region_idx].update_error(error)
 
-            
+        # update region expert
+        self.regions[region_idx].update_expert(self.optim)
+
+        # load region expert off of gpu
+        self.regions[region_idx].expert.to('cpu')
+
+        print("Step: {}, Region: {}, Action: {}, Error: {}".format(self.num_steps, region_idx, action, self.regions[region_idx].get_last_error()))
+
+        # print cached, allocated, and total memory in GB
+        print("Cached Memory: {}, Allocated Memory: {}, Total Memory: {}".format(torch.cuda.memory_cached()/1e9, torch.cuda.memory_allocated()/1e9, torch.cuda.get_device_properties(0).total_memory/1e9))
+
+        # split region if number of samples is greater than threshold
+        if self.regions[region_idx].should_split():
+            print(f"splitting region {region_idx}")
+            region1, region2 = self.regions[region_idx].split()
+            self.regions[region_idx] = region1
+            self.regions.append(region2)
+            self.n_regions += 1
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        
+        self.num_steps += 1
+        
+        return error
+
+
 
 if __name__ == "__main__":
     import gym_super_mario_bros
@@ -119,7 +247,16 @@ if __name__ == "__main__":
 
     iac = IAC(env)
 
-    for i in range(100):
+    error_list = []
+
+    for i in range(1000):
         start = time()
-        iac.step()
-        print(time() - start)
+        error = iac.step()
+        error_list.append(error)
+        # print(error)
+        # print(len(error_list))
+        # print(time() - start)
+    
+    plt.plot(error_list[::10])
+    plt.yscale("log")
+    plt.savefig("asdf.png")
